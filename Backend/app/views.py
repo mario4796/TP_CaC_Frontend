3c9f2f43818760f6a8fd94a27c46dacdd7e031e7
LatIv18 <im.lattanzio@gmail.com>
2024-06-28 17:30:33 -0300

    armo archivos .py

---
+++ Backend/app/views.py
@@ -0,0 +1,48 @@
+from flask import jsonify, request
+from app.models import Book
+
+def get_all_books():
+
+    books = Book.get_all()
+    list_books = [book.serialize() for book in books]
+    return jsonify(list_books)
+
+def create_book():
+    
+    data = request.json
+    new_book = Book(
+        title=data['title'],
+        author=data['author'],
+        genre=data['genre'],
+        publisher=data['publisher']
+    )
+    new_book.save()
+    return jsonify({'message':'Petición creada con exito'}), 201
+    
+def update_book(book_id):
+    
+    book = Book.get_by_id(book_id)
+    if not book:
+        return jsonify({'message': 'Libro no encontrado'}), 404
+    data = request.json
+    book.title = data['title']
+    book.author = data['author']
+    book.genre = data['genre']
+    book.publisher = data['publisher']
+    book.save()
+    return jsonify({'message': 'Libro actualizado exitosamente'})
+
+def get_book(book_id):
+
+    book = Book.get_by_id(book_id)
+    if not book:
+        return jsonify({'message': 'Libro no encontrado'}), 404
+    return jsonify(book.serialize())
+
+def delete_book(book_id):
+
+    book = Book.get_by_id(book_id)
+    if not book:
+        return jsonify({'message': 'Libro no encontrado'}), 404
+    book.delete()
+    return jsonify({'message': 'Libro eliminado exitosamente'})
