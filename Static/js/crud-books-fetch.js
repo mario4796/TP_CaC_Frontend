66dc795ac277e827cf89ac9379104ad304c393e8
LatIv18 <im.lattanzio@gmail.com>
2024-07-02 14:42:54 -0300

    cambios en js para vincular con back en nube

--- Static/js/crud-books-fetch.js
+++ Static/js/crud-books-fetch.js
@@ -1,4 +1,6 @@
-const BASEURL = 'http://127.0.0.1:5000';
+// const BASEURL = 'http://127.0.0.1:5000';
+
+const BASEURL = 'http://ivmalattanzio.pythonanywhere.com/'
 
 /**
  * Función para realizar una petición fetch con JSON.
